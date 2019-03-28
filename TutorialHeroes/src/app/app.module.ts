f2ea6effb358c9cfe74a810f06d17423667fc9aa
Sahrmn <samy32m@gmail.com>
2019-03-28 16:45:44 -0300

    Tutorial heroes

---
+++ TutorialHeroes/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './componentes/heroes/heroes.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';//importar para poder usar ngModel
+import { HeroDetailComponent } from './componentes/hero-detail/hero-detail.component'; 
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule, 
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
